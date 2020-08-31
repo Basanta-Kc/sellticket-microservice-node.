470ed2f83aeaf05ff8a9afbfa36d1741dcc90dd4
Basanta Kc <devbasanta@gmail.com>
2020-08-31 22:33:45 +0545

    ingress nginx setup

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -3,6 +3,9 @@ import express from 'express'
 const app = express()
 app.use(express.json())
 
+app.get('/api/users/currentuser', (req, res) => res.send('hello there'))
+
 app.listen(3000, () => {
-  console.log('Listening on port: 3000')
+  console.log('dfaasdf')
+	console.log('Listening on port: 3000')
 })
