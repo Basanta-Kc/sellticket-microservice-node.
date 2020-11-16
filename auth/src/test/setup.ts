08f010d73834549e4e850a13cc10552e27e81ea2
Basanta-Kc <devbasanta@gmail.com>
2020-11-17 02:02:01 +0545

    change mongo-memory-server  to get test working

--- auth/src/test/setup.ts
+++ auth/src/test/setup.ts
@@ -3,45 +3,47 @@ import { MongoMemoryServer } from 'mongodb-memory-server'
 import mongoose from 'mongoose'
 import { app } from '../app'
 
-jasmine.DEFAULT_TIMEOUT_INTERVAL = 600000
-
+//jasmine.DEFAULT_TIMEOUT_INTERVAL = 600000
 declare global {
-	namespace NodeJS {
-		interface Global {
-			getCookie(): Promise<string[]>
-		}
-	}
+  namespace NodeJS {
+    interface Global {
+      getCookie(): Promise<string[]>
+    }
+  }
 }
 
 let mongo: MongoMemoryServer
 
 beforeAll(async () => {
-	process.env.JWT_KEY = 'FDSA'
-	mongo = new MongoMemoryServer()
-	const mongoUri = await mongo.getUri()
-	await mongoose.connect(mongoUri, {
-		useNewUrlParser: true,
-		useUnifiedTopology: true,
-	})
+  process.env.JWT_KEY = 'FDSA'
+  mongo = new MongoMemoryServer()
+  const mongoUri = await mongo.getUri()
+  await mongoose.connect(mongoUri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
 })
 
 beforeEach(async () => {
-	const collections = await mongoose.connection.db.collections()
-	for (let collection of collections) {
-		await collection.deleteMany({})
-	}
+  const collections = await mongoose.connection.db.collections()
+  for (let collection of collections) {
+    await collection.deleteMany({})
+  }
 })
 
 afterAll(async () => {
-	await mongo.stop()
-	await mongoose.connection.close()
+  try {
+    await mongo.stop()
+    await mongoose.connection.close()
+  } catch (error) {
+    console.log(error)
+  }
 })
 
 global.getCookie = async () => {
-	const res = await request(app)
-		.post('/api/auth/signup')
-		.send({ email: 'test@test.com', password: 'password' })
-		.expect(201)
-	console.log(res.get('Set-Cookie'))
-	return res.get('Set-Cookie')
+  const res = await request(app)
+    .post('/api/auth/signup')
+    .send({ email: 'test@test.com', password: 'password' })
+    .expect(201)
+  return res.get('Set-Cookie')
 }
---
+++ tickets/src/controllers/__test__/create-post.controller.test.ts
@@ -0,0 +1,8 @@
+import request from 'supertest'
+import { app } from '../../app'
+
+it('has post request /api/tickets to create ticket', async () => {})
+it('can only be accessed if user is signed in', async () => {})
+it('returns an error if invalid title is provided', async () => {})
+it('returns an error if invalid price is provided', async () => {})
+it('creates a ticket with valid inputs', async () => {})
