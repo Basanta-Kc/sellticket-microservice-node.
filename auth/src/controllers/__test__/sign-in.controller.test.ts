32af47bdc4dbad9cb7a377d8109e97831cb4eedf
Basanta-Kc <devbasanta@gmail.com>
2020-11-20 23:52:34 +0545

    update 400 code to use statusecode.bad_request

--- auth/src/controllers/__test__/sign-in.controller.test.ts
+++ auth/src/controllers/__test__/sign-in.controller.test.ts
@@ -1,56 +1,57 @@
+import { StatusCodes } from '@arshantechnology/common'
 import request from 'supertest'
 import { app } from '../../app'
 
 it('returns a 201 on succcesful signup', async () => {
-	await request(app)
-		.post('/api/auth/signup')
-		.send({ email: 'test@test.com', password: 'password' })
-		.expect(201)
+  await request(app)
+    .post('/api/auth/signup')
+    .send({ email: 'test@test.com', password: 'password' })
+    .expect(201)
 })
 
-it('returns a 400 wtih invalid email', async () => {
-	await request(app)
-		.post('/api/auth/signup')
-		.send({ email: 'test.com', password: 'password' })
-		.expect(400)
+it('returns a StatusCodes.BAD_REQUEST wtih invalid email', async () => {
+  await request(app)
+    .post('/api/auth/signup')
+    .send({ email: 'test.com', password: 'password' })
+    .expect(StatusCodes.BAD_REQUEST)
 })
 
-it('returns a 400 wtih invalid password', async () => {
-	await request(app)
-		.post('/api/auth/signup')
-		.send({ email: 'test@test.com', password: 'pa' })
-		.expect(400)
+it('returns a StatusCodes.BAD_REQUEST wtih invalid password', async () => {
+  await request(app)
+    .post('/api/auth/signup')
+    .send({ email: 'test@test.com', password: 'pa' })
+    .expect(StatusCodes.BAD_REQUEST)
 })
 
-it('returns a 400 wtih missing email & password', async () => {
-	await request(app)
-		.post('/api/auth/signup')
-		.send({ email: 'test@test.com' })
-		.expect(400)
+it('returns a StatusCodes.BAD_REQUEST wtih missing email & password', async () => {
+  await request(app)
+    .post('/api/auth/signup')
+    .send({ email: 'test@test.com' })
+    .expect(StatusCodes.BAD_REQUEST)
 
-	await request(app)
-		.post('/api/auth/signup')
-		.send({ password: 'password' })
-		.expect(400)
+  await request(app)
+    .post('/api/auth/signup')
+    .send({ password: 'password' })
+    .expect(StatusCodes.BAD_REQUEST)
 })
 
 it('disallows duplicate emails', async () => {
-	await request(app)
-		.post('/api/auth/signup')
-		.send({ email: 'test@test.com', password: 'password' })
-		.expect(201)
+  await request(app)
+    .post('/api/auth/signup')
+    .send({ email: 'test@test.com', password: 'password' })
+    .expect(201)
 
-	await request(app)
-		.post('/api/auth/signup')
-		.send({ email: 'test@test.com', password: 'password' })
-		.expect(400)
+  await request(app)
+    .post('/api/auth/signup')
+    .send({ email: 'test@test.com', password: 'password' })
+    .expect(StatusCodes.BAD_REQUEST)
 })
 
 it('sets a cookie after successful signup', async () => {
-	const res = await request(app)
-		.post('/api/auth/signup')
-		.send({ email: 'test@test.com', password: 'password' })
-		.expect(201)
+  const res = await request(app)
+    .post('/api/auth/signup')
+    .send({ email: 'test@test.com', password: 'password' })
+    .expect(201)
 
-	expect(res.get('Set-Cookie')).toBeDefined()
+  expect(res.get('Set-Cookie')).toBeDefined()
 })
--- auth/src/controllers/__test__/sign-up.controller.test.ts
+++ auth/src/controllers/__test__/sign-up.controller.test.ts
@@ -1,34 +1,35 @@
+import { StatusCodes } from '@arshantechnology/common'
 import request from 'supertest'
 import { app } from '../../app'
 
 it('fails when try to login with invalid email', async () => {
-	await request(app)
-		.post('/api/auth/signin')
-		.send({ email: 'test@test.com', password: 'password' })
-		.expect(400)
+  await request(app)
+    .post('/api/auth/signin')
+    .send({ email: 'test@test.com', password: 'password' })
+    .expect(StatusCodes.BAD_REQUEST)
 })
 
 it('fails when try to login with invalid password', async () => {
-	await request(app)
-		.post('/api/auth/signup')
-		.send({ email: 'test@test.com', password: 'password' })
-		.expect(201)
-	await request(app)
-		.post('/api/auth/signin')
-		.send({ email: 'test@test.com', password: 'password123' })
-		.expect(400)
+  await request(app)
+    .post('/api/auth/signup')
+    .send({ email: 'test@test.com', password: 'password' })
+    .expect(201)
+  await request(app)
+    .post('/api/auth/signin')
+    .send({ email: 'test@test.com', password: 'password123' })
+    .expect(StatusCodes.BAD_REQUEST)
 })
 
 it('response with cookie with valid email & password', async () => {
-	await request(app)
-		.post('/api/auth/signup')
-		.send({ email: 'test@test.com', password: 'password' })
-		.expect(201)
+  await request(app)
+    .post('/api/auth/signup')
+    .send({ email: 'test@test.com', password: 'password' })
+    .expect(201)
 
-	const res = await request(app)
-		.post('/api/auth/signin')
-		.send({ email: 'test@test.com', password: 'password' })
-		.expect(200)
-	
-	expect(res.get('Set-Cookie')).toBeDefined()
+  const res = await request(app)
+    .post('/api/auth/signin')
+    .send({ email: 'test@test.com', password: 'password' })
+    .expect(200)
+
+  expect(res.get('Set-Cookie')).toBeDefined()
 })
